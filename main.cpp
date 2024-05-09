23e571214fecd4fdf450e3040198864774d24b5f
TheKamenski <k.kamenskiy@gmail.com>
2024-05-09 18:22:13 +0300

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    while (num!=0) {
+        std::cout << "I will never lose deadline!" << std::endl;
+	num--;									    }
+
+    return 0;
+}
